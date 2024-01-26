f9327d7adde0808c3be639304069ba323364ac33
Me-Smrt <code_t@krummhere.com>
2024-01-25 20:18:33 -0500

    Working on functions for getting Camera data easily : TK

--- src/main/java/frc/robot/sensors/Camera.java
+++ src/main/java/frc/robot/sensors/Camera.java
@@ -6,14 +6,58 @@ package frc.robot.sensors;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import org.photonvision.PhotonCamera;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class Camera extends SubsystemBase {
-  /** Creates a new Camera. */
-  public Camera() {
-    
-  }
+  
+  private NetworkTableInstance inst = NetworkTableInstance.getDefault();
+  private PhotonCamera april = new PhotonCamera(inst, "april"); 
+  
+  public Camera() {}
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    faceAprilTagID(1);
+  }
+
+  public int getAprilTagID() {
+    /* If this function returns a 0, that means that PhotonVision either isn't configured
+     * correctly, not connected, or their is some other issue (e.g. Camera name isn't matching 
+     * the code).
+     * If this function returns a 1, that means there is not any detected targets
+     */
+    
+    if (inst.getTable("photonvision") != null){
+      if (april.getLatestResult().getBestTarget() != null) {
+        return april.getLatestResult().getBestTarget().getFiducialId();
+      } else {
+        return 1;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  public double getAprilTagYaw() {
+    /* If this function returns a 0, that means that PhotonVision either isn't configured
+     * correctly, not connected, or their is some other issue (e.g. Camera name isn't matching 
+     * the code).
+     * If this function returns a 1, that means there is not any detected targets
+     */
+
+    if (inst.getTable("photonvision") != null){
+      if (april.getLatestResult().getBestTarget() != null) {
+        return april.getLatestResult().getBestTarget().getYaw();
+      } else {
+        return 1;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  public void faceAprilTagID(int ID){
+    System.out.println(getAprilTagID() + " " + getAprilTagYaw());
   }
 }
