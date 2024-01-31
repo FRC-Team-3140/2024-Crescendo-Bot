0ffbe7726ddf24f0372d0507eff94abbb355e554
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2024-01-31 17:12:50 -0500

    Update IntakeShooter.java
    Commented out shooter code so we can test the intake (committed to github because the toughbook wasn't working).

--- IntakeShooter.java
+++ IntakeShooter.java
@@ -13,31 +13,35 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class IntakeAndShooter extends SubsystemBase {
 
     private static IntakeAndShooter instance = null;
-    private CANSparkMax intakeMotor = new CANSparkMax(1, MotorType.kUnbrushed);
-    private CANSparkMax shooterA = new CANSparkMax(2, MotorType.kUnbrushed);
-    private CANSparkMax shooterB = new CANSparkMax(3, MotorType.kUnbrushed);
+    private CANSparkMax intakeMotor = new CANSparkMax(20, MotorType.kBrushless);
+  // private CANSparkMax shooterA = new CANSparkMax(2, MotorType.kUnbrushed);
+   // private CANSparkMax shooterB = new CANSparkMax(3, MotorType.kUnbrushed);
     private DigitalInput noteSensor;
 
+// Returns the instance
     public static synchronized IntakeAndShooter getInstance() {
         if (instance == null) {
             instance = new IntakeAndShooter();
         }
         return instance;
     }
-
+// Sets the motor to neutral
     private IntakeAndShooter() {
-        intakeMotor.setNeutralMode(NeutralMode.Brake);
+        intakeMotor.setIdleMode(IdleMode.kBrake);
     }
 
+// Sets the speed of the intake motor through power
     public void intake(double power) {
-        intakeMotor.set(ControlMode.PercentOutput, power);
+        intakeMotor.set(power);
     }
 
+/*Sets the speed of the shooter's motor, make sure one is negative and one is postive
     public void shoot(double power) {
         shooterA.set(ControlMode.PercentOutput, -power);
-        shooterB.set(ControlMode.PercentOutput, -power);
-    }
+        shooterB.set(ControlMode.PercentOutput, power);
+    } */
 
+// Not filled out yet, will do when shooter/intake is done
     public boolean getNoteSensor() {
         return noteSensor.get();
     }
