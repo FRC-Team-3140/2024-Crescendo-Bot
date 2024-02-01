f7a4e90bdd749b85b4c86310ad5b1408effbbcc5
Me-Smrt <code_t@krummhere.com>
2024-01-31 21:00:45 -0500

    Tried to implement Sysid things; Need to change some values in there-PN

--- src/main/java/frc/robot/subsystems/IntakeShooter.java
+++ src/main/java/frc/robot/subsystems/IntakeShooter.java
@@ -5,19 +5,79 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import static edu.wpi.first.units.Units.Meters;
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.RobotController;
+import static edu.wpi.first.units.Units.Volts;
+import static edu.wpi.first.units.MutableMeasure.mutable;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.*;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+
 
 public class IntakeShooter extends SubsystemBase {
 
     public  static IntakeShooter instance = null;
     public CANSparkMax intakeMotor = new CANSparkMax(20, MotorType.kBrushless);
-    private CANSparkMax shooterA = new CANSparkMax(9, MotorType.kBrushless);
-    private CANSparkMax shooterB = new CANSparkMax(8, MotorType.kBrushless);
+    public CANSparkMax shooterA = new CANSparkMax(9, MotorType.kBrushless);
+    public CANSparkMax shooterB = new CANSparkMax(8, MotorType.kBrushless);
     // private DigitalInput noteSensor;
+    public RelativeEncoder encoderA = shooterA.getEncoder();
+    public RelativeEncoder encoderB = shooterB.getEncoder();
+
+    
+    
+    private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
+    // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+    private final MutableMeasure<Distance> m_distance = mutable(Meters.of(0));
+    // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+    private final MutableMeasure<Velocity<Distance>> m_velocity = mutable(MetersPerSecond.of(0));
+
+    SysIdRoutine routine = new SysIdRoutine(
+          new SysIdRoutine.Config(),
+          new SysIdRoutine.Mechanism(
+              // Tell SysId how to plumb the driving voltage to the motors.
+              (Measure<Voltage> volts) -> {
+                shooterA.setVoltage(volts.in(Volts));
+                shooterB.setVoltage(volts.in(Volts));
+              },
+              // Tell SysId how to record a frame of data for each motor on the mechanism being
+              // characterized.
+              log -> {
+                // Record a frame for the left motors.  Since these share an encoder, we consider
+                // the entire group to be one motor.
+                log.motor("drive-left")
+                    .voltage(
+                        m_appliedVoltage.mut_replace(
+                            shooterA.get() * RobotController.getBatteryVoltage(), Volts))
+                    .linearPosition(m_distance.mut_replace(m_leftEncoder.getDistance(), Meters))
+                    .linearVelocity(
+                        m_velocity.mut_replace(m_leftEncoder.getRate(), MetersPerSecond));
+                // Record a frame for the right motors.  Since these share an encoder, we consider
+                // the entire group to be one motor.
+                log.motor("drive-right")
+                    .voltage(
+                        m_appliedVoltage.mut_replace(
+                            shooterB.get() * RobotController.getBatteryVoltage(), Volts))
+                    .linearPosition(m_distance.mut_replace(m_rightEncoder.getDistance(), Meters))
+                    .linearVelocity(
+                        m_velocity.mut_replace(m_rightEncoder.getRate(), MetersPerSecond));
+              },
+              // Tell SysId to make generated commands require this subsystem, suffix test state in
+              // WPILog with this subsystem's name ("drive")
+              this));   
+
 
 // Returns the instance
     public static synchronized IntakeShooter getInstance() {
@@ -33,14 +93,18 @@ public class IntakeShooter extends SubsystemBase {
 
 // Sets the speed of the intake motor through power
     public void intake(double power) {
-        intakeMotor.set(power);
+        intakeMotor.setVoltage(power);
     }
 
 //Sets the speed of the shooter's motor, make sure one is negative and one is postive
-    public void shoot(double power) {
-        shooterA.set(-power);
-        shooterB.set(power);
+    public void voltageDrive(double power) {
+        shooterA.setVoltage(-power);
+        shooterB.setVoltage(power);
     } 
+
+    public void logMotors(){
+
+    }
 
 // Not filled out yet, will do when shooter/intake is done
     // public boolean getNoteSensor() {
@@ -49,8 +113,7 @@ public class IntakeShooter extends SubsystemBase {
 
     @Override
     public void periodic() {
-    // SysIdRoutine routine = new SysIdRoutine(new SysIdRoutine.Config(), new SysIdRoutine.Mechanism(this::voltageDrive, this::logMotors, this));
     }
+
+
 }
-
-
