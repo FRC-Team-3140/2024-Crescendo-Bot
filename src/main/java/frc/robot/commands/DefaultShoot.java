6a3b59904a2c780a057b352b6e1d95c313e90c76
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2024-02-04 16:00:12 -0500

    Took out unnecessary lines in DefaultShoot -GP

--- src/main/java/frc/robot/commands/DefaultShoot.java
+++ src/main/java/frc/robot/commands/DefaultShoot.java
@@ -2,7 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// Made AmpShoot the Y keybind on the xbox controller so we can test it
+// Made DefaultShoot the B keybind on the Xbox controller so we can test it
+// This command is meant so that we can input the speeds
 
 package frc.robot.commands;
 
@@ -10,19 +11,21 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.IntakeShooter;
 
-public class AmpShoot extends Command implements Constants{
+public class DefaultShoot extends Command implements Constants {
 
-    public IntakeShooter intakeShooter; 
+    private final IntakeShooter intakeShooter;
+    private final double voltage;
 
-    public AmpShoot() {
-        intakeShooter = IntakeShooter.getInstance();
-
+    public DefaultShoot(double voltage) {
+        this.intakeShooter = IntakeShooter.getInstance();
+        this.voltage = voltage;
+      // Adjust the desiredVoltage variable to the voltage value you want to use. 
+      // You can then use this instance of DefaultShoot in your robot's command scheduler or bind it to a button as needed for your specific control setup.
     }
 
-      // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    intakeShooter.setShooterVoltage(3);
-  }
-
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        intakeShooter.setShooterVoltage(voltage);
+    }
 }
