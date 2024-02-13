d985b319cf1be5cd554cc0c2c8539d49f3316d90
Me-Smrt <code_t@krummhere.com>
2024-02-12 19:11:57 -0800

    SR-removed limit switches, added solenoids

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,7 +4,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -13,31 +12,29 @@ public class Climber extends SubsystemBase{
     private CANSparkMax leftClimber;
     private CANSparkMax rightClimber;
 
-    //Limit Switches
-    private DigitalInput rightUpperLimit;
-    private DigitalInput rightLowerLimit;
-    private DigitalInput leftUpperLimit;
-    private DigitalInput leftLowerLimit;
-
     //Solenoids - Probably does not work
     private DigitalOutput leftSolenoid;
     private DigitalOutput rightSolenoid;
+
+    //CAN IDs
+    private int leftCANID = 14;
+    private int rightCANID = 15;
+
+    //Relay ports
+    private int leftSolenoidID = 0;
+    private int rightSolenoidID = 1;
     
-    //left motor CAN ID, right motor CAN ID, limit switches DIO port IDs, solenoid relay port IDs
-    public Climber(int leftCANID, int rightCANID, int rightUpperLimitID,int rightLowerLimitID, int leftUpperLimitID, int leftLowerLimitID, int leftSolenoidID, int rightSolenoidID){
+    //climber
+    public Climber(){
         leftClimber = new CANSparkMax(leftCANID, MotorType.kBrushless);
         rightClimber = new CANSparkMax(rightCANID, MotorType.kBrushless);
-
-        rightUpperLimit = new DigitalInput(rightUpperLimitID);
-        rightLowerLimit = new DigitalInput(rightLowerLimitID);
-        leftUpperLimit = new DigitalInput(leftUpperLimitID);
-        leftLowerLimit = new DigitalInput(leftLowerLimitID);
-
+;
+        //electromagnetic push-pull solenoids
         leftSolenoid = new DigitalOutput(leftSolenoidID);
         rightSolenoid = new DigitalOutput(rightSolenoidID);
 
 
-        //set motors
+        //set motor settings
         leftClimber.setIdleMode(IdleMode.kBrake);
         rightClimber.setIdleMode(IdleMode.kBrake);
 
@@ -51,54 +48,43 @@ public class Climber extends SubsystemBase{
 
     /*
      * Raising and lowering climbers
-     * Checks if the limit switch is pressed. If it is, sets motor to 0. If not, sets motor to a speed.
-     * 
      */
+    //raises the left climber
+    public void raiseLeft(){
+        
+        leftClimber.set(.1); //change to an actual value later
+        
+    }
 
     //raises the right climber
     public void raiseRight(){
-        if (rightUpperLimit.get()) {
-            // We are going up and top limit is tripped so stop
-            rightClimber.set(0);
-        } else {
-            // We are going up but top limit is not tripped so go at commanded speed
-            rightClimber.set(.1); //change to an actual value later
-        }
-    }
-
-    //raises the left climber
-    public void raiseLeft(){
-        if (leftUpperLimit.get()) {
-            // We are going up and top limit is tripped so stop
-            leftClimber.set(0);
-        } else {
-            // We are going up but top limit is not tripped so go at commanded speed
-            leftClimber.set(.1); //change to an actual value later
-        }
-    }
-
-    //lowers the right climber
-    public void lowerRight(){
-        if (rightLowerLimit.get()) {
-            // We are going up and top limit is tripped so stop
-            rightClimber.set(0);
-        } else {
-            // We are going up but top limit is not tripped so go at commanded speed
-            rightClimber.set(-.1); //change to an actual value later
-        }
+        
+        rightClimber.set(.1); //change to an actual value later
+        
     }
 
     //lowers the left climber
     public void lowerLeft(){
-        if (leftLowerLimit.get()) {
-            // We are going up and top limit is tripped so stop
-            leftClimber.set(0);
-        } else {
-            // We are going up but top limit is not tripped so go at commanded speed
-            leftClimber.set(-.1); //change to an actual value later
-        }
+        leftClimber.set(-.1); //change to an actual value later
+        
     }
 
+    //lowers the right climber
+    public void lowerRight(){
+        rightClimber.set(-.1); //change to an actual value later
+        
+    }
+
+    
+    public void stopLeft(){
+        leftClimber.set(0);
+    }
+
+    public void stopRight(){
+        rightClimber.set(0);
+    }
+
+    //These will probably never be used
     //raises both left and right climbers
     public void raiseBoth(){
         raiseLeft();
@@ -110,6 +96,7 @@ public class Climber extends SubsystemBase{
         lowerLeft();
         lowerRight();
     }
+
 }
 
 
