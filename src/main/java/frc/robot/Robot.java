159f6546df0ac3f4f82d14bbdf02af4823f12d59
Me-Smrt <code_t@krummhere.com>
2024-03-21 18:51:35 -0400

    Updated PhotonVision Library, Going to add Camera settings and Calibration files to GitHub after calibration is complete - TK

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import org.littletonrobotics.junction.LoggedRobot;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.pickupNote;
 import frc.robot.subsystems.Arm;
 
 public class Robot extends LoggedRobot implements Constants {
@@ -162,6 +163,7 @@ public class Robot extends LoggedRobot implements Constants {
     // AutoBuilder.buildAuto("Turn")
     // ).schedule();
 
+    new pickupNote(true, RobotContainer.swerve, RobotContainer.intake, RobotContainer.camera).schedule();
   }
 
   /** This function is called periodically during test mode. */
