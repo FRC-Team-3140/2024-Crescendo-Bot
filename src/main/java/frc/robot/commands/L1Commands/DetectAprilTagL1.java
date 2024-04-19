355f41258033095866b6649cd9a3ebd166a009bb
Me-Smrt <code_t@krummhere.com>
2024-04-19 17:33:15 -0400

    Converted DetectAprilTagL1 command to new AprilTagMeasurement object - TK

--- src/main/java/frc/robot/commands/L1Commands/DetectAprilTagL1.java
+++ src/main/java/frc/robot/commands/L1Commands/DetectAprilTagL1.java
@@ -1,18 +1,17 @@
 package frc.robot.commands.L1Commands;
 
-import org.photonvision.targeting.PhotonTrackedTarget;
-
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 import frc.robot.sensors.Camera;
+import frc.robot.sensors.Camera.AprilTagMeasurement;
 
 /**
  * Detect the april tag
  * 
- * Positive yaw values mean turn the robot left.  Negitive yaw values mean turn the robot right.
+ * Positive yaw values mean turn the robot left. Negitive yaw values mean turn
+ * the robot right.
  * 
  * Distance seems to be along the ground and over estimates by about 7 percent.
  */
@@ -28,8 +27,8 @@ public class DetectAprilTagL1 extends Command {
     private double m_end_time;
     private double m_timeout;
     private int tag_id = 0;
-    PhotonTrackedTarget target;
-    
+    AprilTagMeasurement target;
+
     /**
      * This command is responsible for detecting an AprilTag in Level 1.
      * It takes a timeout value as a parameter.
@@ -42,11 +41,12 @@ public class DetectAprilTagL1 extends Command {
         // Use addRequirements() here to declare subsystem dependencies.
         // addRequirements(RobotContainer.m_subsystem);
     }
-    
+
     /**
      * Initializes the DetectAprilTagL1 command.
      * This method is called once when the command is first scheduled.
-     * It sets up the necessary variables and assigns the tag ID based on the alliance color.
+     * It sets up the necessary variables and assigns the tag ID based on the
+     * alliance color.
      */
     @Override
     public void initialize() {
@@ -56,15 +56,16 @@ public class DetectAprilTagL1 extends Command {
         m_angle_sum = 0;
         m_count = 0;
 
-        if(DriverStation.getAlliance().get().equals(Alliance.Blue)){
-            tag_id = 7; 
-        }else{
+        if (DriverStation.getAlliance().get().equals(Alliance.Blue)) {
+            tag_id = 7;
+        } else {
             tag_id = 4;
         }
     }
-    
+
     /**
-     * Executes the command to detect an AprilTag and update the running sum of distance and angle.
+     * Executes the command to detect an AprilTag and update the running sum of
+     * distance and angle.
      * If the pose of the detected tag is ambiguous, the measurement is skipped.
      * 
      * @throws Exception if an error occurs during the execution of the command.
@@ -72,34 +73,34 @@ public class DetectAprilTagL1 extends Command {
     @Override
     public void execute() {
         try {
-            
-            target = m_camera.latestAprilTagDetection(tag_id);
-            
+
+            target = m_camera.getAprilTagData(tag_id);
+
             if (target != null) {
-            double ambiguity = target.getPoseAmbiguity();
-            if(ambiguity > 0.1) {
+                double ambiguity = target.ambiguity;
+                if (ambiguity > 0.1) {
+                    RobotContainer.controller.setRumble().schedule();
+                    RobotContainer.controller2.setRumble().schedule();
+                    // Skip this measurement if the pose is ambiguous
+                    return;
+                }
+                ;
+                double x = target.Xdistance;
+                double y = target.Ydistance;
+                double distance = Math.sqrt(x * x + y * y);
+                // double angle = Math.atan2(y, x);
+                double yaw = target.yaw;
+
+                // Update the running sum
+                m_distance_sum += distance;
+                m_angle_sum += yaw;
+                m_count++;
+
+            } else {
                 RobotContainer.controller.setRumble().schedule();
                 RobotContainer.controller2.setRumble().schedule();
-                // Skip this measurement if the pose is ambiguous
-                return;
             }
-            Transform3d pose = target.getBestCameraToTarget();
-            double x = pose.getX();
-            double y = pose.getY();
-            double distance = Math.sqrt(x * x + y * y);
-            // double angle = Math.atan2(y, x);
-            double yaw = target.getYaw();
-            
-            // Update the running sum
-            m_distance_sum += distance;
-            m_angle_sum += yaw;
-            m_count++;
-            
-            }else{
-                RobotContainer.controller.setRumble().schedule();
-                RobotContainer.controller2.setRumble().schedule();
-            }
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.err.println(e + "Line 92");
         }
     }
@@ -109,27 +110,29 @@ public class DetectAprilTagL1 extends Command {
      *
      * @return The PhotonTrackedTarget object representing the detected target.
      */
-    public PhotonTrackedTarget getTarget(){
+    public AprilTagMeasurement getTarget() {
         return target;
     }
 
     /**
-     * Calculates and returns the average distance based on the sum of distances and the count of measurements.
+     * Calculates and returns the average distance based on the sum of distances and
+     * the count of measurements.
      * 
      * @return The average distance.
      */
     public double getDistance() {
         return m_distance_sum / m_count;
-    }   
+    }
 
     /**
-     * Returns the average yaw angle calculated from the sum of angles and the count of measurements.
+     * Returns the average yaw angle calculated from the sum of angles and the count
+     * of measurements.
      *
      * @return the average yaw angle
      */
     public double getYawAngle() {
         return m_angle_sum / m_count;
-    }   
+    }
 
     /**
      * Returns the count of values in the average.
@@ -139,7 +142,7 @@ public class DetectAprilTagL1 extends Command {
     public int count() {
         return m_count;
     }
-    
+
     /**
      * This method is called when the command ends.
      * 
@@ -148,15 +151,17 @@ public class DetectAprilTagL1 extends Command {
     @Override
     public void end(boolean interrupted) {
     }
-    
+
     /**
      * Checks if the command has finished executing.
-     * The command is considered finished if either the count exceeds 2 or the current time exceeds the end time.
+     * The command is considered finished if either the count exceeds 2 or the
+     * current time exceeds the end time.
+     * 
      * @return true if the command is finished, false otherwise.
      */
     @Override
     public boolean isFinished() {
-        if (m_count > 2 ) {
+        if (m_count > 2) {
             return true;
         }
 
@@ -166,5 +171,5 @@ public class DetectAprilTagL1 extends Command {
 
         return false;
     }
-    
+
 }
