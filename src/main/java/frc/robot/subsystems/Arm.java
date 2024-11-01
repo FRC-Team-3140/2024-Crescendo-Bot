e39d263f76d28a59acc66d1e78102cdca8322e5b
Me-Smrt <code_t@krummhere.com>
2024-11-01 16:56:48 -0400

    Improvements to Camera paths for Demonstration

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -175,13 +175,13 @@ public class Arm extends SubsystemBase {
     angleInterpolator = new InterpolatingDoubleTreeMap();// Add your inverseInterpolator, interp2lator, and comparator
                                                          // here
 
-    angleInterpolator.put(0.0, 12.0);
+    angleInterpolator.put(0.0, 15.0);
     angleInterpolator.put(1.3, 20.0);
-    angleInterpolator.put(2.3, 26.0);
-    angleInterpolator.put(3.3 , 34.0);
-    angleInterpolator.put(4.4, 35.0); // Maximum effective range
-    angleInterpolator.put(5.6, 35.0); // Max apriltag range
-    angleInterpolator.put(6.6, 35.0);
+    angleInterpolator.put(2.3, 29.0);
+    angleInterpolator.put(3.3 , 37.0);
+    angleInterpolator.put(4.4, 28.0); // Maximum effective range
+    angleInterpolator.put(5.6, 28.0); // Max apriltag range
+    angleInterpolator.put(6.6, 28.0);
 
     // Set the arm to disabled by default.
     disable();
